610a9a29c101f7cfe2ea19357443ffdffa7bfdd3
Nick Browning <56777652+nickbrowningdev@users.noreply.github.com>
2021-05-05 13:56:50 +1000

    Add files via upload

---
+++ PersonCRUD.Library/IPersonORM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonCRUD.Library
+{
+    public interface IPersonORM
+    {
+        void PersonCreate(string personFirstName, string personLastName, string personEmail, string personPhone);
+        void PersonUpdate(int personID, string personFirstName, string personLastName, string personEmail, string personPhone);
+        void PersonDelete(int personID);
+        List<Person> ReadAllPeople();
+        List<Person> SearchPersonFirstName(string personFirstName);
+    }
+}
