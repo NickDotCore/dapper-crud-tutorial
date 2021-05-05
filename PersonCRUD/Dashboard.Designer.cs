4670d48434e15625fd445352b97e9038b971018e
Nick Browning <56777652+nickbrowningdev@users.noreply.github.com>
2021-05-05 13:59:12 +1000

    Add files via upload

---
+++ PersonCRUD/Dashboard.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace PersonCRUD
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtSearchFirstName = new System.Windows.Forms.TextBox();
+            this.btnSavePerson = new System.Windows.Forms.Button();
+            this.btnDeletePerson = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearchFirstName = new System.Windows.Forms.Button();
+            this.dataDashboard = new System.Windows.Forms.DataGridView();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDashboard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(78, 16);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(247, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(78, 46);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(247, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(78, 77);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(247, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(78, 109);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(247, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtSearchFirstName
+            // 
+            this.txtSearchFirstName.Location = new System.Drawing.Point(118, 207);
+            this.txtSearchFirstName.Name = "txtSearchFirstName";
+            this.txtSearchFirstName.Size = new System.Drawing.Size(207, 20);
+            this.txtSearchFirstName.TabIndex = 4;
+            // 
+            // btnSavePerson
+            // 
+            this.btnSavePerson.Location = new System.Drawing.Point(12, 176);
+            this.btnSavePerson.Name = "btnSavePerson";
+            this.btnSavePerson.Size = new System.Drawing.Size(101, 23);
+            this.btnSavePerson.TabIndex = 5;
+            this.btnSavePerson.Text = "Save Person";
+            this.btnSavePerson.UseVisualStyleBackColor = true;
+            this.btnSavePerson.Click += new System.EventHandler(this.btnSavePerson_Click);
+            // 
+            // btnDeletePerson
+            // 
+            this.btnDeletePerson.Location = new System.Drawing.Point(118, 176);
+            this.btnDeletePerson.Name = "btnDeletePerson";
+            this.btnDeletePerson.Size = new System.Drawing.Size(101, 23);
+            this.btnDeletePerson.TabIndex = 6;
+            this.btnDeletePerson.Text = "Delete Person";
+            this.btnDeletePerson.UseVisualStyleBackColor = true;
+            this.btnDeletePerson.Click += new System.EventHandler(this.btnDeletePerson_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(224, 176);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearchFirstName
+            // 
+            this.btnSearchFirstName.Location = new System.Drawing.Point(12, 205);
+            this.btnSearchFirstName.Name = "btnSearchFirstName";
+            this.btnSearchFirstName.Size = new System.Drawing.Size(101, 23);
+            this.btnSearchFirstName.TabIndex = 8;
+            this.btnSearchFirstName.Text = "Search";
+            this.btnSearchFirstName.UseVisualStyleBackColor = true;
+            this.btnSearchFirstName.Click += new System.EventHandler(this.btnSearchFirstName_Click);
+            // 
+            // dataDashboard
+            // 
+            this.dataDashboard.AllowUserToAddRows = false;
+            this.dataDashboard.AllowUserToDeleteRows = false;
+            this.dataDashboard.AllowUserToOrderColumns = true;
+            this.dataDashboard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataDashboard.Location = new System.Drawing.Point(347, 12);
+            this.dataDashboard.Name = "dataDashboard";
+            this.dataDashboard.ReadOnly = true;
+            this.dataDashboard.Size = new System.Drawing.Size(473, 267);
+            this.dataDashboard.TabIndex = 9;
+            this.dataDashboard.DoubleClick += new System.EventHandler(this.dataDashboard_DoubleClick);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(12, 19);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblFirstName.TabIndex = 10;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(12, 49);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(61, 13);
+            this.lblLastName.TabIndex = 11;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 80);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 12;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(12, 112);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(41, 13);
+            this.lblPhone.TabIndex = 13;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 307);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.dataDashboard);
+            this.Controls.Add(this.btnSearchFirstName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDeletePerson);
+            this.Controls.Add(this.btnSavePerson);
+            this.Controls.Add(this.txtSearchFirstName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataDashboard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtSearchFirstName;
+        private System.Windows.Forms.Button btnSavePerson;
+        private System.Windows.Forms.Button btnDeletePerson;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSearchFirstName;
+        private System.Windows.Forms.DataGridView dataDashboard;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+    }
+}
---
+++ PersonCRUD/Dashboard.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PersonCRUD.Library;
+
+namespace PersonCRUD
+{
+    public partial class Dashboard : Form
+    {
+        // create Person object
+        Person person = new Person();
+
+        // create People list
+        List<Person> people = new List<Person>();
+
+        PersonDAL personDAL = new PersonDAL();
+
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        // fills out data grid view
+        private void FillDataGridView()
+        {
+            try
+            {
+                people = personDAL.ReadAllPeople();
+                dataDashboard.DataSource = people;
+                dataDashboard.Columns[0].Visible = false;
+
+                ClearTextboxes();
+                DisableDeleteButton();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FillDataGridView_Search()
+        {
+            try
+            {
+                people = personDAL.SearchPersonFirstName(txtSearchFirstName.Text);
+                dataDashboard.DataSource = people;
+                dataDashboard.Columns[0].Visible = false;
+
+                ClearTextboxes();
+                DisableDeleteButton();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // clears textboxes in dashboard
+        private void ClearTextboxes()
+        {
+            txtFirstName.Text = ""; 
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+            txtSearchFirstName.Text = "";
+
+            person.PersonID = 0;
+        }
+
+        // disables delete button
+        private void DisableDeleteButton()
+        {
+            btnDeletePerson.Enabled = false;
+        }
+
+        // enables delete button
+        private void EnableDeleteButton()
+        {
+            btnDeletePerson.Enabled = true;
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+                DisableDeleteButton();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSavePerson_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (person.PersonID != 0)
+                {
+                    personDAL.PersonUpdate(person.PersonID, txtFirstName.Text, txtLastName.Text, txtEmail.Text, txtPhone.Text);
+                }
+                else
+                {
+                    personDAL.PersonCreate(txtFirstName.Text, txtLastName.Text, txtEmail.Text, txtPhone.Text);
+                }
+
+                ClearTextboxes();
+                DisableDeleteButton();
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDeletePerson_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                personDAL.PersonDelete(person.PersonID);
+
+                ClearTextboxes();
+                DisableDeleteButton();
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearTextboxes();
+                DisableDeleteButton();
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearchFirstName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView_Search();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataDashboard_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int personID = 0;
+
+                if(dataDashboard.CurrentRow.Index != -1)
+                {
+                    personID = Convert.ToInt32(dataDashboard.CurrentRow.Cells[0].Value.ToString());
+                    txtFirstName.Text = dataDashboard.CurrentRow.Cells[1].Value.ToString();
+                    txtLastName.Text = dataDashboard.CurrentRow.Cells[2].Value.ToString();
+                    txtEmail.Text = dataDashboard.CurrentRow.Cells[3].Value.ToString();
+                    txtPhone.Text = dataDashboard.CurrentRow.Cells[4].Value.ToString();
+
+                    person.PersonID = personID;
+                    person.PersonFirstName = txtFirstName.Text;
+                    person.PersonLastName = txtLastName.Text;
+                    person.PersonEmail = txtEmail.Text;
+                    person.PersonPhone = txtPhone.Text;
+
+                    EnableDeleteButton();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
